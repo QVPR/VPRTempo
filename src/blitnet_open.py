83a7553a20afa3985d964d4ac553d5aaa47d5e1f
Adam Hines <adam.hines@qut.edu.au>
2023-07-10 17:18:22 +1000

    Adding P@100R and sum of absolute difference cross-validation

--- src/blitnet_open.py
+++ src/blitnet_open.py
@@ -1,6 +1,6 @@
 #MIT License
 
-#Copyright (c) 2023 Adam Hines
+#Copyright (c) 2023 Adam Hines, Peter Stratton, Michael Milford, Tobias Fischer
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
