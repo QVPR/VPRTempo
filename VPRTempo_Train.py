0f045339de32c8c904461a220420b5e0376c567d
Adam Hines <adam.hines@qut.edu.au>
2023-11-06 15:47:49 +1000

    Combined the inh and exc weights for inferencing in non-quantized version

--- VPRTempo_Train.py
+++ VPRTempo_Train.py
@@ -36,6 +36,7 @@ sys.path.append('./dataset')
 import blitnet as bn
 import numpy as np
 import torch.nn as nn
+import torchvision.transforms as transforms
 
 from settings import configure, model_logger
 from dataset import CustomImageDataset, ProcessImage
@@ -179,6 +180,22 @@ class VPRTempo(nn.Module):
         spikes = layer.exc(spikes) + layer.inh(spikes)
         
         return spikes
+
+    def combine_weights(self, model):
+        for layer_name, _ in sorted(model.layer_dict.items(), key=lambda item: item[1]):
+            # Retrieve the layer object
+            layer = getattr(model, layer_name)
+            dims = layer.dims
+            # Define weight variable in layer
+            layer.w = nn.Linear(dims[0],dims[1],bias=False)
+            # Send to model device
+            layer.x.to(model.device)
+            # Replace weights with combined weights
+            layer.w.weight = nn.Parameter(layer.exc.weight + layer.inh.weight)
+            # Delete original weights
+            del layer.exc, layer.inh
+
+        return model   
     
     def save_model(self, model_out):    
         """
@@ -216,7 +233,9 @@ def train_new_model(model, model_name):
     :param qconfig: Quantization configuration
     """
     # Initialize the image transforms and datasets
-    image_transform = ProcessImage(model.dims, model.patches)
+    image_transform = transforms.Compose([
+        ProcessImage(model.dims, model.patches)
+    ])
     train_dataset = CustomImageDataset(annotations_file=model.dataset_file, 
                                        img_dirs=model.training_dirs,
                                        transform=image_transform,
@@ -244,6 +263,8 @@ def train_new_model(model, model_name):
         model.train_model(train_loader, layer, prev_layers=trained_layers)
         # After training the current layer, add it to the list of trained layers
         trained_layers.append(layer_name)
+    # Combine excitatory and inhibitory weights
+    model = model.combine_weights(model)
     # Convert the model to a quantized model
     model.eval()
     # Save the model
