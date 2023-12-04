fec3f2593095116350c008f6ea7af08970dcfe98
Adam Hines <adam.hines@qut.edu.au>
2023-12-04 10:24:35 +1000

    Modify version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [
 # define the setup
 setup(
     name="VPRTempo",
-    version="1.1.0",
+    version="1.1.1",
     description='VPRTempo: A Fast Temporally Encoded Spiking Neural Network for Visual Place Recognition',
     long_description=long_description,
     long_description_content_type='text/markdown',
